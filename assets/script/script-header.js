8c75b7c00ce15b0415d48b6512b46bfd85c3f651
Amandine PIMENTEL DE ANDRADE <21041626@GRN164.local>
2021-08-17 16:02:25 +0200

    Version 13 (connexion/deconnexion avec gestion du header opé 17082021)

--- assets/script/script-header.js
+++ assets/script/script-header.js
@@ -3,44 +3,46 @@ $(document).ready(function() {
 
     let header = `
     <header>
-        <button id="back" class="invisible">Retour</button>
-            <h1 class="visible">Centre culturel des Marmusots</h1>
-        <div id="main-menu" class="visible">
-            <select name="fond" id="fond">
-                <option value="Catalogue" disabled selected>Catalogue</option>
-            </select>
-            <select name="nav-bibliothecaire" id="nav-bibliothecaire" class="visible">
-                <option value="Gestion adhérents" disabled selected>Gestion adhérents</option>
-                <option value="Nouvel adhérent">Nouvel adhérent</option>
-                <option value="Gestion compte">Gestion compte</option>
-            </select>
-            <select name="nav-gestionnaire" id="nav-gestionnaire" class="invisible">
-                <option value="Gestion du fond" disabled selected>Gestion du fond</option>
-                <option value="Ajouter une BD">Ajouter une BD</option>
-                <option value="Rechercher une BD">Rechercher une BD</option>
-            </select>
-            <select name="nav-gestion-emp" id="nav-gestion-emp" class="invisible">
-                <option value="Gestion des emplacements" disabled selected>Gestion des emplacements</option>
-            </select>
-            <select name="nav-responsable" id="nav-responsable" class="invisible">
-                <option value="Statistiques" disabled selected>Statistiques</option>
-                <option value="BD empruntées">BD empruntées</option>
-                <option value="Autres statistiques">Autres statistiques</option>
-            </select>
+        <div class="invisible" id="header-div">
+            <button id="back" class="invisible">Retour</button>
+                <h1 class="invisible">Centre culturel des Marmusots</h1>
+            <div id="main-menu" class="invisible">
+                <select name="fond" id="fond">
+                    <option value="Catalogue" disabled selected>Catalogue</option>
+                </select>
+                <select name="nav-bibliothecaire" id="nav-bibliothecaire" class="invisible">
+                    <option value="Gestion adhérents" disabled selected>Gestion adhérents</option>
+                    <option value="Nouvel adhérent">Nouvel adhérent</option>
+                    <option value="Gestion compte">Gestion compte</option>
+                </select>
+                <select name="nav-gestionnaire" id="nav-gestionnaire" class="invisible">
+                    <option value="Gestion du fond" disabled selected>Gestion du fond</option>
+                    <option value="Ajouter une BD">Ajouter une BD</option>
+                    <option value="Rechercher une BD">Rechercher une BD</option>
+                </select>
+                <select name="nav-gestion-emp" id="nav-gestion-emp" class="invisible">
+                    <option value="Gestion des emplacements" disabled selected>Gestion des emplacements</option>
+                </select>
+                <select name="nav-responsable" id="nav-responsable" class="invisible">
+                    <option value="Statistiques" disabled selected>Statistiques</option>
+                    <option value="BD empruntées">BD empruntées</option>
+                    <option value="Autres statistiques">Autres statistiques</option>
+                </select>
+            </div>
         </div>
         <!-- Compte placeholder-->
     </header>
     `
 
     let compte = `
-    <div id="compte">
-        <p id="identification" class="visible">Bonjour Testitest !</p>      <!--Attention enlever Testitest-->
-        <select name="nav" id="nav" class="invisible">
+    <div id="compte" class = "visible">
+        <p id="identification" class="invisible">Bonjour Testitest !</p>      <!--Attention enlever Testitest-->
+        <select name="nav" id="nav-adherent" class="invisible">
             <option value="" disabled selected>Mon compte</option>
             <option value="mafiche">Ma fiche adhérent</option>
-            <option value="logout">Se déconnecter</option>
         </select>
-        <button id="connexion" class="invisible">Se connecter</button>
+        <button id="deconnexion" class="invisible">Se déconnecter</button>
+        <button id="connexion" class="visible" >Se connecter</button>
     </div>
     `
     
