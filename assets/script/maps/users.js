b72c48a5ff51d6ad8c4d8e403f0739d5a62b056e
Amandine PIMENTEL DE ANDRADE <21041626@GRN164.local>
2021-08-18 16:30:26 +0200

    Version 1000000 (journée du 18082021)

--- assets/script/maps/users.js
+++ assets/script/maps/users.js
@@ -6,7 +6,7 @@ var users = new Map();
 // 3 = Adhérent
 // 4 = Responsable
 
-users.set("1", {idA : "clement.lepecheur", mdp: "Gp3t3d3s0l3", role : 4});
+users.set("1", {idA : "clement.lepecheur", mdp: "Gp3t3d3s0l3", role : 1});
 users.set("2", {idA: "aurelie.menestrelle", mdp: "J3u5u15dr0l3", role : 3});
 users.set("3", {idA: "francois.licorne", mdp:"gr4d13ntl1n341r", role:2});
 users.set("4", {idA: "vendredi.laplusmims", mdp:"m4rc3l1n", role:4});
