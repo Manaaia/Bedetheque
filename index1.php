3824025b2a07f809d11c32dc2c7a0e019feec191
Flavie Féray <flavie.feray@gmail.com>
2021-09-26 21:00:14 +0200

    version avec modif recherche adherent

--- index1.php
+++ index1.php
@@ -4,30 +4,47 @@ if(isset($_GET["action"])) {
     $action = $_GET["action"];
 }
 switch ($action) {
+    // page de connexion
     case "connexion":
         require("assets/vues/view_connexion.php");
         break;
+
+    // page d'accueil avec menu dynamique une fois connecté
     case "cptAccueil":
         require("assets/vues/view_cpt_accueil.php");
         break;
+
+    // page de recherche d'une BD
     case "rechercheBD":
+        $titreForm = "Rechercher une BD";
         require("assets/vues/view_recherche_ISBN.php");
         break;
+
+    // page d'ajout d'une BD
     case "addBD":
         require("assets/vues/view_ajouter_BD.php");
         break;
+
+    // page d'ajout d'un Exemplaire
     case "addExp":
         require("assets/vues/view_ajouter_exemplaire.php");
         break;
+
+    // page de création d'adhérent
     case "createAdh":
         require("assets/vues/view_creer_adherent.php");
         break;
+
+    // page de fiche d'adhérent
     case "ficheAdh":
         require("assets/vues/view_fiche_adherent.php");
         break;
+
+    // page de fiche de BD
     case "ficheBD":
         require("assets/vues/view_fiche_BD.php");
         break;
+        
     case "gestAdh":
         require("assets/vues/view_gestion_adherent.php");
         break;
