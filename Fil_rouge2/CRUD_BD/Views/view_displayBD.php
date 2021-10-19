6a7cb80113893496508b135ddd316129796f477e
Flavie Feray <flavie.feray@gmail.com>
2021-10-19 16:25:06 +0200

    vue display base html

---
+++ Fil_rouge2/CRUD_BD/Views/view_displayBD.php
@@ -0,0 +1,84 @@
+<div class="container">
+        <fieldset class="form"><legend for="form">Fiche BD</legend>
+            <div id="rechercheExemplaire">
+                <p class="label">ISNB : </p>
+                <p id="isbn"></p>
+            </div>
+            <hr>
+            <div class="visible" id="container-bd" >
+                <div id="ajoutBD" class="visible" >
+                    <div id="alertesaisie" class="alerte invisible"></div>
+                    <div>
+                        <p class="label">Code exemplaire :</p>
+                        <p id="codeBD"></p>
+                    </div>
+
+                    <br>
+
+                    <div>
+                        <p class="label">Titre :</p>
+                        <p id="titreBD">
+                    </div>
+
+                    <br/>
+
+                    <div>
+                        <p class="label">Auteur :</p>
+                        <p id="selectAuteur"></p>
+                    </div>
+
+                    <br/>
+                                
+                    <div>
+                        <p class="label">Série :</p>
+                        <p id="selectSerie">
+                        </p>
+                    </div>
+
+                    <br>
+                    <div>
+                        <p class="label">État :</p>
+                        <p id="etatBD">
+                        </p>
+                    </div>
+
+                    <br>
+                    <div>
+                        <p class="label">Bibliothèque :</p>
+                        <p id="biblioExemplaire"></p>
+                    </div>
+                    <br>
+                    <div>
+                        <p class="label">Emplacement :</p>
+                        <p id="emplacementBD"></p>
+                    </div>
+                    <br>
+                    <div>
+                        <p class="label">Commentaires :</p>
+                        <p id="commentaireBD">Neque porro quisquam est qui dolorem ipsum quia dolor 
+                            sit amet, consectetur, adipisci velit...</p>
+                    </div>
+                    <br>
+                    <div>
+                        <p class="label">Résumé :</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+
+                    <br>
+                </div>
+                <div id="th2">
+                    <img id="imgBD">
+                </div>
+            </div>
+                
+                <div id="btns">
+                    <button id="modifierBD">Modifier</button>
+                    <button id="ajouterExemplaire">Ajouter Exemplaire</button>
+                </div>
+            </div> 
+        </fieldset>        
+    </div>
