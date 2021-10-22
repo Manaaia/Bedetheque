743507998f5e45741bde4df3b9521680fe13b569
Manaaia <85735324+Manaaia@users.noreply.github.com>
2021-10-22 09:11:16 +0200

    Delete maps + Correction BDD 221020210911

--- Fil_rouge2/Contenu/assets/script/maps/biblios.js
+++
@@ -1,7 +0,0 @@
-var biblios = new Map();
-biblios.set("0", {nom:"Alexis de Tocqueville"});
-biblios.set("1", {nom:"Chemin-vert"});
-biblios.set("2", {nom:"Folie-CouvreChef"});
-biblios.set("3", {nom:"Grâce-de-Dieu"});
-biblios.set("4", {nom:"Guérinière"});
-biblios.set("5", {nom:"Maladrerie"});
